1058dee19bc472d36631c8cc5f5d083f3f7f2ee1
watce001 <watce001@mymail.unisa.edu.au>
2016-05-14 19:41:19 +0930

    Other Things!

---
+++ Effulgent/Intermediate/Build/Win64/UE4Editor/Development/Effulgent/Effulgent.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_EFFULGENT_H__
+#define __AUTO_EFFULGENT_H__
+//Last Write: 14/05/2016 10:06:15 AM +00:00
+#include "D:\Caroline\Documents\GitHub\Game-Design\Effulgent\Source\Effulgent\Effulgent.h"
+#endif//__AUTO_EFFULGENT_H__
